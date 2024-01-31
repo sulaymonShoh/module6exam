de6829f412e1fb23c9322ab94c297fe0fcd2a3f6
Sulaymonqori <ms13061312@gmail.com>
2024-01-31 20:13:06 +0500

    Homepage uchun view

--- sulaymonqori/views.py
+++ sulaymonqori/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, DetailView, DeleteView, UpdateView, ListView
 
-# Create your views here.
+from sulaymonqori.models import Post
+
+
+class HomeView(View):
+    def get(self, request):
+        posts_for_homepage = Post.objects.order_by('-created_at')[:10]
+        context = {
+            "posts": posts_for_homepage,
+        }
+
+        return render(request, 'home.html', context=context)
