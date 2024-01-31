711fe86cd3a7cdfc59c33ff9c9a936e79e5b0ce0
Sulaymonqori <ms13061312@gmail.com>
2024-01-31 20:10:56 +0500

    AbstractBaseModel, Post modellari yozildi

--- sulaymonqori/models.py
+++ sulaymonqori/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AbstractBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(AbstractBaseModel):
+    title = models.CharField(max_length=120)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
